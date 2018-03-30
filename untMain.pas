602eac96df26a35c8057b3d35102e2302cd12376
Alexandre Xavier <alexandrebxavier@hotmail.com>
2018-03-30 08:58:21 -0400

    Connection

--- untMain.pas
+++ untMain.pas
@@ -4,10 +4,15 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.Phys.MSSQL, FireDAC.Phys.MSSQLDef, FireDAC.FMXUI.Wait,
+  Data.DB, FireDAC.Comp.Client;
 
 type
   TfrmMain = class(TForm)
+    FDConnection: TFDConnection;
   private
     { Private declarations }
   public
